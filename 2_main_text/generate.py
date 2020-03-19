87d8c469ee9b8f30dcf5239b2fb2fda4386663ec
matanki.saito <matanki.saito@gmail.com>
2020-03-20 04:03:19 +0900

    Pythonファイルの整形

--- 2_main_text/generate.py
+++ 2_main_text/generate.py
@@ -2,57 +2,57 @@ import os
 import subprocess
 
 
-def generateBMFont(name, ext, addSourceFile=False):
+def generate_bm_font(name, ext, add_source_file):
     # コマンド
-    cmdList = [
+    cmd_list = [
         '../1_tool/bmfont64.exe',
         '-c', "./bmfc/" + name + ext,
         '-o', "./out/" + name + ".fnt"
-    ];
+    ]
 
     # 追加ファイルあり
-    if (addSourceFile):
-        cmdList.append('-t');
-        cmdList.append(addSourceFile);
+    if add_source_file:
+        cmd_list.append('-t')
+        cmd_list.append(add_source_file)
 
     # BFFont実行
-    subprocess.call(" ".join(cmdList));
+    subprocess.call(" ".join(cmd_list))
 
 
-def readTextFile(name):
+def read_text_file(name):
     f = open('./source/' + name + '.txt', "r", encoding='utf_8_sig')
-    text = f.read();
-    f.close();
-    return text;
+    text = f.read()
+    f.close()
+    return text
 
 
 def main():
-    sourceText = "";
+    source_text = ""
 
     # sourceフォルダ内を走査
-    files = os.listdir("./source");
+    files = os.listdir("./source")
     for file in files:
         # 拡張子とファイル名を分離
-        file_name, file_ext = os.path.splitext(file);
+        file_name, file_ext = os.path.splitext(file)
 
         # 拡張子がtxt
-        if (file_ext == ".txt"):
-            sourceText = sourceText + readTextFile(file_name);
+        if file_ext == ".txt":
+            source_text = source_text + read_text_file(file_name)
 
     # source textを作成
-    f = open('source.txt', 'w', encoding='utf_8_sig');
-    f.write(sourceText);
-    f.close();
+    f = open('source.txt', 'w', encoding='utf_8_sig')
+    f.write(source_text)
+    f.close()
 
     # bmfcフォルダ内を走査
-    files = os.listdir("./bmfc");
+    files = os.listdir("./bmfc")
     for file in files:
         # 拡張子とファイル名を分離
-        file_name, file_ext = os.path.splitext(file);
+        file_name, file_ext = os.path.splitext(file)
 
         # 拡張子がbmfcの時のみフォントを生成
-        if (file_ext == ".bmfc"):
-            generateBMFont(file_name, file_ext, "source.txt");
+        if file_ext == ".bmfc":
+            generate_bm_font(file_name, file_ext, "source.txt")
 
 
 if __name__ == "__main__":
